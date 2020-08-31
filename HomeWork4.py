c7c8306c75f36818375def23a5ab216ab3a41dc5
yunyiliu <yunyiliu@usc.edu>
2020-08-31 14:04:10 +0800

    Create HomeWork4.py

---
+++ HomeWork4.py
@@ -0,0 +1,9 @@
+'''Download the data (http://ai.stanford.edu/~amaas/data/sentiment/)
+Process the data ( clean it, tokenize it ) 
+Build word embedding 
+Build neural network classification 
+Train the model 
+Calculate accuracy as AUCROC 
+Reference: https://www.tensorflow.org/tutorials/text/text_classification_rnn  ( do not copy their code , code every step yourself)  (feel free to use pytorch!)
+It is ok if your model performance is low.
+'''
